4b2111f0e48055a4e173436cab1afbbd615d599e
SilentByte4096 <srikkarthikey@gmail.com>
2024-04-16 00:43:33 +0530

    yello

--- java/main.java
+++ java/main.java
@@ -3,5 +3,6 @@ package java;
 public class main {
     public static void main(String[] args) {
         System.out.print("Can you read this ?");
+        System.out.println("What's woring with VS Code?");
     }
 }
