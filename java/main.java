9c2078b069b1f13990e5abcfa49790e9e2ef642a
vedant-maheshwari <vedantmaheshwari23@gmail.com>
2024-04-16 00:39:25 +0530

    krishna

--- java/main.java
+++ java/main.java
@@ -3,5 +3,6 @@ package java;
 public class main {
     public static void main(String[] args) {
         System.out.print("Can you read this ?");
+        System.out.println("yes krishna i can read  it.");
     }
 }
