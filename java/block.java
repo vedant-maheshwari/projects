97b78819f0f885e22397b6e8e400dfac1377c31c
vedant-maheshwari <vedantmaheshwari23@gmail.com>
2024-04-16 13:09:08 +0530

    blockchain

--- java/block.java
+++ java/block.java
@@ -1,4 +1,51 @@
 package java;
+<<<<<<< HEAD
 public class block {
     
 }
+=======
+import java.util.Arrays;
+
+public class Block {
+    private String[] transactions;
+    private int blockHash;
+    private int previousBlockHash;
+    
+    @Override
+    public String toString() {
+        return "Block [transactions=" + Arrays.toString(transactions) +", blockHash=" + blockHash + 
+        ", previousBlockHash=" + previousBlockHash +"]";
+    }
+    
+    public Block(String[] transactions, int previousBlockHash) {
+        super();
+        this.transactions = transactions;
+        this.previousBlockHash = previousBlockHash;
+        this.blockHash = Arrays.hashCode( new int[] { Arrays.hashCode(Arrays.toString(transactions).getBytes()), this.previousBlockHash});
+    }
+    
+    public String[] getTransactions () {
+        return transactions;
+    }
+    
+    public void setTransactions (String[] transactions) {
+        this.transactions = transactions;
+    }
+    
+    public int getBlockHash() {
+        return blockHash;
+    }
+    
+    public void setBlockHash(int blockHash) {
+        this.blockHash = blockHash;
+    }
+    
+    public int getPreviousBlockHash() {
+        return previousBlockHash;
+    }
+    
+    public void setPreviousBlockHash(int previousBlockHash) {
+        this.previousBlockHash = previousBlockHash;
+    }
+}
+>>>>>>> 25f5ac5 (blockchain)
